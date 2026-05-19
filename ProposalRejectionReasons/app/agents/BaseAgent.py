81435b1db8b3ed4890e4d5c3e1b255814187b24b
unknown <ahmedragab000170@gmail.com>
2026-05-19 16:28:58 +0300

    Adding Basic Configurations & Modifying requirements.txt

---
+++ ProposalRejectionReasons/app/agents/BaseAgent.py
@@ -0,0 +1,6 @@
+# --------------------------------------------------------
+# A general class used to build agents
+# --------------------------------------------------------
+
+from langchain.agents import create_agent
+from langchain.chat_models import init_chat_model
