725ff9c0c72e6540602687029cad27c1bf896eb4
AAhmeddRRagabb <ahmedragab000170@gmail.com>
2026-05-31 03:18:58 +0300

    Adding General Result Schema for any Sub-agent

---
+++ ProposalRejectionReasons/app/schemas/final_subagents_schema/final_subagents_schema.py
@@ -0,0 +1,28 @@
+# ---------------------------------------------------------------------------------------
+# The final schema shoud be returned by subagents + post-processing after agents calling
+# ---------------------------------------------------------------------------------------
+
+
+from pydantic import BaseModel, Field, ConfigDict
+from typing import Annotated
+
+Score   = Annotated[float, Field(ge = 0.0, le = 1.0)]
+Reason  = Annotated[str, Field(min_length = 10, max_length = 50)]
+Summary = Annotated[str, Field(min_length = 25, max_length = 100)]
+
+class SubagentResult(BaseModel):
+    """
+    General Result schema for any sub-agent
+    """
+    model_config = ConfigDict(
+        strict = True,
+        validate_assignment = True,
+        extra = "forbid"
+    )
+
+    score: Score
+    accepted: bool
+    summary: Summary
+
+    acceptance_reasons: list[Reason] | None = None
+    rejection_reasons : list[Reason] | None = None
